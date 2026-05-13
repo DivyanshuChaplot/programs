a3f06fdaed89149e44620363373b4a0035425423
DivyanshuChaplot <jaindivyanshu41@gmail.com>
2026-05-13 08:29:25 +0530

    Add manipulator , constant and precedence

---
+++ c++imp1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+//    int a=23;
+//    cout<<"the value of a was: "<<a<<endl;
+//    a=24;
+//    cout<<"the value of a is: "<<a<<endl;
+
+    //*******************constant in c++***************
+    // const  int a=23;
+    //cout<<"the value of a was: "<<a<<endl;
+    //a=24; //----> here a is constant so this is not execute
+    //cout<<"the value of a is: "<<a<<endl;
+
+    // manipulators in c++
+
+    // int a=2,b=34,c=1234;
+    // cout<<"value of a without setw is :"<<a<<endl;
+    // cout<<"value of b without setw is :"<<b<<endl;
+    // cout<<"value of c without setw is :"<<c<<endl;
+
+    // cout<<"value of a with setw is: "<<setw(4)<<a<<endl;
+    // cout<<"value of b with setw is: "<<setw(4)<<b<<endl;
+    // cout<<"value of c with setw is: "<<setw(4)<<c<<endl;
+
+    // operator precedence******
+    
+    //  int a=1,b=23;
+    //  int c = ((((a*b)-8)+45)-23); //--->there is concept of precedence
+    //  cout<<"the value of c is: "<<c;
+    //  return 0;
+}
